79821676f7cbb25fc1ccace53003105a08e8df37
O.R.Asogwaowners <ora0002@auburn.edu>
2016-10-16 21:20:22 -0500

    Another another commit

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -1,9 +1,206 @@
 '''
-Created on Oct 9, 2016
+Started creation on Oct 9, 2016
 
-@author: owners
+@author: Rosemary
 '''
-class Fix:
+import os
+from xml.etree import cElementTree as ET
+from datetime import  date, time
+import math
+class Fix():
+
+ 
+    def __init__(self,logFile = "log.txt"):
+        if (logFile == None):
+            raise ValueError("Fix.__init__:  Hey! You did not insert any File")
+        if (not ( isinstance(logFile, str))):
+            raise ValueError("Fix.__init__:  Hey! The parameter is not a string")
+        if (len(logFile) < 1):
+            raise ValueError("Fix.__init__:  Hey! The Length of file is less than one.")
+        self.logFile = logFile
+        self.sightfile = None
+        self.writeinfilelog('Start of log\n')
+        pass;
+
+
+    def setSightingFile(self,sightingFile = None):
+        if (sightingFile == None):
+            raise ValueError("Fix.setSightingFile:  Hey! There is no file")
+        if (not ( isinstance(sightingFile, str))):
+            raise ValueError("Fix.setSightingFile:  Hey! The parameter is not a string")    
+        filename, file_extension = os.path.splitext(sightingFile)
+        if (len(filename)< 1 or file_extension != '.xml'):
+            raise ValueError("Fix.setSightingFile:  Hey! The file name violates the parameter specification.")
+        if (os.path.exists(sightingFile)):
+            self.writeinfilelog("Start of sighting file " +  sightingFile + "\n")
+        else:
+            raise ValueError("Fix.setSightingFile:  Hey! The File does not exist.")
+        self.sightfile = sightingFile
+        return sightingFile
+
+# ---------------------------------------------------------
+
+# ---------------------------------------------------------
+    def getSightings(self):
+        if (self.sightfile == None):
+            raise ValueError("Fix.getSightings:  Hey! The Sighting File dose Not setup yet.")
+            
+        approximateLatitude = "0d0.0"            
+        approximateLongitude = "0d0.0"  
+        with open (self.sightfile, "r") as myfile:
+            xmlstr=myfile.read() 
+        
+        try:
+            if (self.iscontain_valid(xmlstr)):
+                root = ET.fromstring(xmlstr)
+                if (root.tag == 'fix'):
+                    sight = []
+                    for page in root:
+                        
+# --------------------------------------------------------------------------                        
+                        if (page.find('body') == None):
+                            raise ValueError("Fix.getSightingsFile:  Hey! No body")
+                        body = page.find('body').text
+# --------------------------------------------------------------------------                        
+                        if (page.find('date') == None):
+                            raise ValueError("Fix.getSightingsFile:  Hey! No date")
+                        da = page.find('date').text
+                        d = da.split('-')[2]
+                        mm = da.split('-')[1]
+                        y = da.split('-')[0]
+                        date1 = date(int(y),int(mm),int(d))
+# --------------------------------------------------------------------------                        
+                        if (page.find('time') == None):
+                            raise ValueError("Fix.getSightingsFile:  Hey! No time")
+                        t = page.find('time').text
+                        h = t.split(':')[0]
+                        m = t.split(':')[1]
+                        s = t.split(':')[2]
+                        time1 = time(int(h),int(m),int(s))
+# --------------------------------------------------------------------------                        
+                        if (page.find('observation') == None):
+                            raise ValueError("Fix.getSightingsFile:  Hey! No observation")
+                        al = page.find('observation').text
+                        aa1  = int (al.split('d')[0]) 
+                        aa2  = float(al.split('d')[1] )
+                        if ( (aa2 + aa1) <= 0 ):
+                            raise ValueError("Fix.getSightingsFile:  Hey! The altitude is Less than 0.1")
+                        if ( aa1 < 0 or aa1 >= 90):
+                            raise ValueError("Fix.getSightingsFile:  Hey! The degree portion of the altitude is out of range")
+                        if ( aa2 < 0 or aa2 >= 60):
+                            raise ValueError("Fix.getSightingsFile: Hey! The minute portion of the altitude is out of range")
+                        observation =  str(aa1)+"d"+str(aa2)
+# --------------------------------------------------------------------------                        
+                        if (page.find('height') == None):
+                            height = 0
+                        else: 
+                            height = page.find('height') .text
+                        try :
+                            x = float(height)
+                            if (x < 0 ):
+                                raise ValueError("Fix.getSightingsFile:  Hey! No temperature")
+                        except ValueError:                        
+                                raise ValueError("Fix.getSightingsFile:  Hey! No temperature")
+                        height = float(height)
+# --------------------------------------------------------------------------                        
+                        if (page.find('temperature') == None):
+                            temperature = -72
+                        else:
+                            temperature = page.find('temperature').text
+                            
+                        try:
+                            x = int (temperature)
+                            if (x < -20 or x > 120 ):
+                                raise ValueError("Fix.getSightingsFile:  Hey! Temperature is out of the range")                        
+                        except ValueError:
+                                raise ValueError("Fix.getSightingsFile:  Hey! Temperature is not an Integer")
+                        temperature = int(temperature)                        
+# --------------------------------------------------------------------------                        
+                        if (page.find('pressure') == None):
+                            pressure = 1010
+                        else:
+                            pressure = page.find('pressure').text   
+                                                    
+                        try:
+                            x= int(pressure)
+                            if (x < 100 or x > 1100):
+                                raise ValueError("Fix.getSightingsFile:  Hey! Pressure is out of the range")                        
+                        except ValueError:
+                                raise ValueError("Fix.getSightingsFile:  Hey! Pressure is not an Integer")                        
+                        pressure = int(pressure)  
+# --------------------------------------------------------------------------                        
+                        if (page.find('horizon') == None):
+                            horizon = "natural"
+                        horizon = page.find('horizon').text                       
+                        if (horizon.lower() != "natural" and horizon.lower() != "artificial"):
+                            raise ValueError("Fix.getSightingsFile:  Hey! No horizon")                        
+# --------------------------------------------------------------------------                        
+                        try :
+                            X = (body ,date1 , time1, observation, height, temperature, pressure, horizon)
+                            sight.append(X)
+                        except ValueError:
+                            raise ValueError("Fix.getSightingsFile:  Hey! The file contains invalid information")
+                else:
+                    raise ValueError("Fix.getSightingsFile:  Hey! The file contains invalid information")
+            else:
+                    raise ValueError("Fix.getSightingsFile:  Hey! The file contains invalid information")
+                    
+        except ValueError:
+            raise ValueError("Fix.getSightingsFile:  Hey! The file contains invalid information")
+
+        ss = sight
+        ss = sorted(ss,key=lambda x: (x[1],x[2],x[0]))
+        self.write_sight(ss)
+        self.writeinfilelog ("End of sighting file "+self.sightfile + "\n")
+
+        return (approximateLatitude,approximateLongitude)
+        
+ 
+        
+    def iscontain_valid(self, root= None):
+        if (root == None):
+            return False
+        if ('fix' in root and 'sighting' in root):
+            return True
+        else:
+            return False    
     
-    def __init__(self):
+    def writeinfilelog (self, s):
+        try:
+            t = datetime.now()
+            correctformula = datetime(t.year, t.month, t.day, t.hour, t.minute , t.second ,  tzinfo=TZ()).isoformat(' ')
+             
+            totalstring = "LOG: "+correctformula+"\t"+s
+            f = open(self.logFile, 'a')
+            f.write(totalstring)
+        except ValueError:
+            raise ValueError("Fix.__init__:  Hey! The file name violates the parameter specification.")
         pass
+  
+    def write_sight(self, sight):
+        for x in sight:
+            sightstring = str( x[0]) + "\t" + str( x[1].strftime('%Y-%m-%d') ) + "\t"  + str( x[2].strftime('%H:%M:%S')) + "\t"  + str( self.Calculate_altitud(x) + "\n") 
+            self.writeinfilelog (sightstring )
+    
+    def Calculate_altitud(self,x):
+        if (x[7].lower() == "natural"):
+            dip = ( -0.97 * math.sqrt( x[4]) ) / 60            
+        else:
+            dip = 0
+        z= x[3].split("d")
+        alt = float(z[0]) + (float(z[1])/ 60)
+        refraction = ( -0.00452 * float(x[6])) / ( 273 +  ((x[5] - 32) * (5/9) ) ) / math.tan( math.radians(alt) )            
+        adjustedAltitude = alt + dip + refraction 
+        a = int(adjustedAltitude -  adjustedAltitude%1) 
+        b = "d"  
+        c = round( (adjustedAltitude %1) *60 , 1 ) 
+        if (len(str(c)) == 3):
+            c = "0"+str(c)
+        d = str(a)+b+str(c)
+        return d
+
+
+from datetime import tzinfo, timedelta, datetime
+class TZ(tzinfo):
+    def utcoffset(self, dt): 
+        return timedelta(hours=-6)    
--- SoftwareProcess/Navigation/test/Fixtest.py
+++ SoftwareProcess/Navigation/test/Fixtest.py
@@ -1,11 +1,194 @@
 '''
-Created on Oct 9, 2016
+Started creation on Oct 9, 2016
 
-@author: owners
+@author: Rosemary
 '''
+import unittest
 import Navigation.prod.Fix as Fix
 
+class Test(unittest.TestCase):
+  
+     
 
-theFix = Fix.Fix()     
-theFix.setSightingFile("sightings.xml")        
-approximatePosition = theFix.getSightings()        
+# --------------constructor-----------------------
+# happy path
+    def test100_010_ShouldConstructFix(self):
+        self.assertIsInstance(Fix.Fix(), Fix.Fix)
+
+    def test100_020_ShouldConstructFix(self):
+        self.assertIsInstance(Fix.Fix("hi"), Fix.Fix)
+
+
+# sad path
+    def test100_910_ShouldnotConstructFix(self):
+        with self.assertRaises(ValueError):
+            Fix.Fix(None)
+            
+    def test100_920_ShouldnotConstructFix(self):
+        test_var = 0 ; 
+        with self.assertRaises(ValueError):
+            Fix.Fix(test_var)
+
+    def test100_930_ShouldnotConstructFix(self):
+        test_var = "" ; 
+        with self.assertRaises(ValueError):
+            Fix.Fix(test_var)
+
+# -----------setSightingFile-------------------------
+# happy path
+    def test200_010_ReturnTheNameOfFile(self):
+        F = Fix.Fix()
+        self.assertEqual(F.setSightingFile("hi.xml"),"hi.xml")
+# sad path
+    def test200_910_NoFileExist(self):
+        F = Fix.Fix()
+        test_var = "bye.xml" ; 
+        with self.assertRaises(ValueError):
+            F.setSightingFile(test_var)
+
+    def test200_920_PassIntegerAsFile(self):
+        F = Fix.Fix()
+        test_var = 1; 
+        with self.assertRaises(ValueError):
+            F.setSightingFile(test_var)
+
+    def test200_930_PassNone(self):
+        F = Fix.Fix()
+        with self.assertRaises(ValueError):
+            F.setSightingFile()
+
+    def test200_940_passFilewithlengthlessthanone(self):
+        F = Fix.Fix()
+        test_var = ""; 
+        with self.assertRaises(ValueError):
+            F.setSightingFile(test_var)
+  
+      
+    def test200_950_PassfileWithNoExtention(self):
+        F = Fix.Fix()
+        test_var = "hi"; 
+        with self.assertRaises(ValueError):
+            F.setSightingFile(test_var)
+            
+    def test200_960_PassfileWithdifferentExtention(self):
+        F = Fix.Fix()
+        test_var = "hi.txt"; 
+        with self.assertRaises(ValueError):
+            F.setSightingFile(test_var)
+# ---------------------setSightingFile-----------------------
+# happy path
+    def test300_010_correct(self):
+        F = Fix.Fix()
+        F.setSightingFile("hi.xml")
+        self.assertEqual(F.getSightings(),('0d0.0','0d0.0'))
+
+    def test300_020_Sortsamedate(self):
+        F = Fix.Fix("samedatelog.txt")
+        F.setSightingFile("sortsamedate.xml")
+        self.assertEqual(F.getSightings(),('0d0.0','0d0.0'))
+
+    def test300_030_Sortsamedateandtime(self):
+        F = Fix.Fix("samedateandtimelog.txt")
+        F.setSightingFile("sortsamedateandtime.xml")
+        self.assertEqual(F.getSightings(),('0d0.0','0d0.0'))
+
+
+
+# sad path
+    def test300_910_notsetupsetsighting(self):
+        F = Fix.Fix()
+        with self.assertRaises(ValueError):
+            F.getSightings()
+            
+    def test300_920_altitudeLessThanrange(self):
+        F = Fix.Fix()
+        F.setSightingFile("altitudelessthan1.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()
+
+    def test300_930_missbody(self):
+        F = Fix.Fix()
+        F.setSightingFile("missBody.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()
+         
+    def test300_940_missdate(self):
+        F = Fix.Fix()
+        F.setSightingFile("missDate.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()         
+
+    def test300_950_misstime(self):
+        F = Fix.Fix()
+        F.setSightingFile("missTime.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()           
+    
+    def test300_960_missObservation(self):
+        F = Fix.Fix()
+        F.setSightingFile("missobservation.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()
+
+    def test300_970_Observationhavedegreemorethan90(self):
+        F = Fix.Fix()
+        F.setSightingFile("obgreatthan90.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()        
+
+
+    def test300_980_Observationhavedegreelessthan0(self):
+        F = Fix.Fix()
+        F.setSightingFile("oblessthan0ford.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()   
+            
+
+    def test300_990_Observationhaveminutelessthan0(self):
+        F = Fix.Fix()
+        F.setSightingFile("oblessthan0form.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()    
+            
+    def test300_991_Observationhaveminutegreatthan60(self):
+        F = Fix.Fix()
+        F.setSightingFile("obgreatthan60.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()      
+            
+    def test300_992_HeightLessthan0(self):
+        F = Fix.Fix()
+        F.setSightingFile("heightlessthan0.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()      
+            
+    def test300_993_TemperatureMoreThan120(self):
+        F = Fix.Fix()
+        F.setSightingFile("tempmprethan120.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings() 
+            
+    def test300_994_TemperatureLessthan_20(self):
+        F = Fix.Fix()
+        F.setSightingFile("templessthan-20.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()    
+            
+            
+    def test300_995_PressureLessthan100(self):
+        F = Fix.Fix()
+        F.setSightingFile("preslessthan100.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()        
+            
+    def test300_995_Pressuremorethan1100(self):
+        F = Fix.Fix()
+        F.setSightingFile("presmorethan1100.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()   
+            
+    def test300_996_HorizonwithdifferentName(self):
+        F = Fix.Fix()
+        F.setSightingFile("horisdifferentname.xml")
+        with self.assertRaises(ValueError):
+            F.getSightings()       
